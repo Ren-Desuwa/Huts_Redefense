e8d4bd65d2676f1848d3c658abcce4dafb2f4ddc
lol <125685532+Ren-Desuwa@users.noreply.github.com>
2025-05-17 23:49:33 +0800

    models update

--- src/model/Reading.java
+++ src/model/Reading.java
@@ -1,5 +1,44 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Reading {
+	private int user_id;
+	private LocalDate date;
+	private String type; // "Electricity", "Water", "Gas"
+	private int reading;
+	private int rate;
+	private int total_price;
 	
+	public Reading(int user_id, LocalDate date, String type, int reading, int rate, int total_price) {
+		this.user_id = user_id;
+		this.date = date;
+		this.type = type;
+		this.reading = reading;
+		this.rate = rate;
+		this.total_price = total_price;
+	}
+	
+	public Reading(int user_id, LocalDate date, String type, int reading, int total_price) {
+		this.user_id = user_id;
+		this.date = date;
+		this.type = type;
+		this.reading = reading;
+		this.total_price = total_price;
+	}
+	
+	
+	public int getUser_id() {return user_id;}
+	public LocalDate getDate() {return date;}
+	public String getType() {return type;}
+	public int getReading() {return reading;}
+	public int getRate() {return rate;}
+	public int getTotal_price() {return total_price;}
+	
+	public void setUser_id(int user_id) {this.user_id = user_id;}
+	public void setDate(LocalDate date) {this.date = date;}
+	public void setType(String type) {this.type = type;}
+	public void setReading(int reading) {this.reading = reading;}
+	public void setRate(int rate) {this.rate = rate;}
+	public void setTotal_price(int total_price) {this.total_price = total_price;}
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,32 @@
 package model;
 
 public class User {
-	
+    private int user_id;
+    private String username;
+    private String password;
+    private String email;
+    
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    
+    public User(int id, String username, String password, String email) {
+        this.user_id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+    
+    public int getId() {return user_id;}
+    public String getUsername() {return username;}
+    public String getPassword() {return password;}
+    public String getEmail() {return email;}
+    
+    public void setId(int id) {this.user_id = id;}
+    public void setPassword(String password) {this.password = password;}
+    public void setUsername(String username) {this.username = username;}
+    public void setEmail(String email) {this.email = email;}
+    
 }
