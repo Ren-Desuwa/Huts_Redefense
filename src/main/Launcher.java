3a7194531cbad9333b5f1366616b6d3223131df1
lol <125685532+Ren-Desuwa@users.noreply.github.com>
2025-05-17 23:36:31 +0800

    major upload

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -1,7 +1,22 @@
 package main;
 
+import java.awt.EventQueue;
+import view.login.Login_Window;
+
 public class Launcher {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		// TODO Auto-generated method stub
+		// Launch the application
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					// Create and show the main window
+					Login_Window window = new Login_Window();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
