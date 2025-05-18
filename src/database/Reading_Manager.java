bc7e421f75c9150ece390cae943c865a0252d616
lol <125685532+Ren-Desuwa@users.noreply.github.com>
2025-05-18 23:52:40 +0800

    changes?

--- src/database/Reading_Manager.java
+++ src/database/Reading_Manager.java
@@ -240,4 +240,5 @@ public class Reading_Manager {
 		}
 		return list;
 	}
+	
 }
--- src/module-info.java
+++ src/module-info.java
@@ -8,4 +8,5 @@ module Huts_Redefense {
 	requires java.desktop;
 	requires java.sql;
 	requires org.knowm.xchart;
+	requires org.junit.jupiter.api;
 }
