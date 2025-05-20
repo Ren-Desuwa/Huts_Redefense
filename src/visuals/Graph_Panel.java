6423f3dfef235ffb4b994d1a66a44a2278a2a952
Huuujji <Degenerate1456@gmail.com>
2025-05-21 02:07:35 +0800

    Updated Colors

--- src/visuals/Graph_Panel.java
+++ src/visuals/Graph_Panel.java
@@ -81,8 +81,8 @@ public class Graph_Panel extends JPanel {
         overall_graph = new Scrollable_Bar_Graph_Panel("Monthly Total Expenses", "Month", "Php");
 
         // Set bar colors
-        electricity_graph.setBarColor(new Color(79, 129, 189));
-        water_graph.setBarColor(new Color(155, 187, 89));
+        electricity_graph.setBarColor(new Color(213,205,0));
+        water_graph.setBarColor(new Color(79, 129, 189));
         gas_graph.setBarColor(new Color(192, 80, 77));
         overall_graph.setBarColor(new Color(128, 100, 162));
 
@@ -117,8 +117,8 @@ public class Graph_Panel extends JPanel {
 
         Scrollable_Bar_Graph_Panel utility_graph = new Scrollable_Bar_Graph_Panel("", "Month", units);
         Color barColor = switch (utility_type) {
-            case "electricity" -> new Color(79, 129, 189);
-            case "water" -> new Color(155, 187, 89);
+            case "electricity" -> new Color(213,205,0);
+            case "water" -> new Color(79, 129, 189);
             case "gas" -> new Color(192, 80, 77);
             default -> Color.GRAY;
         };
